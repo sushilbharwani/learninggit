2c4554d57970deb252aa1d794bf252beaf41de89
Bharwani <sb185107@ncr.com>
2021-04-21 13:35:04 +0530

    trying pull request

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Git anf React
+          Learn Git anf React from Sushil
         </a>
       </header>
     </div>
