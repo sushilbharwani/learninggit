578b375926e4612c5b1ef608c43cfc15a95bba23
Bharwani <sb185107@ncr.com>
2021-04-23 06:47:35 +0530

    trying rebase

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Git
+          Learn Git for first time
         </a>
       </header>
     </div>
